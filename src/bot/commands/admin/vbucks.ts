d63b436f9bdf5de6ff3447d065e2691e99e779bf
Kuno Claes(D) <kuno@icodes.dev>
2023-05-24 01:09:48 +0200

    Added update vbucks to new db

--- src/bot/commands/admin/vbucks.ts
+++ src/bot/commands/admin/vbucks.ts
@@ -1,12 +1,12 @@
 import { Hash } from "crypto";
 import { EmbedBuilder, PermissionFlagsBits } from "discord.js";
-
+import { DataTypes, Model, Sequelize, json } from 'sequelize';
 export { }
 
 const { SlashCommandBuilder } = require('discord.js');
 const functions = require('../../../structs/functions.js');
-const Users = require('../../../model/user');
-const Profiles = require('../../../model/profiles');
+const Users = require('../../../model/user-gres');
+const Profiles = require('../../../model/profiles-gres');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -28,26 +28,42 @@ module.exports = {
 
         const selectedUser = interaction.options.getUser('user');
         const selectedUserId:Number = selectedUser.id;
+		let currentVbucks = 0, newVbucks = 0;
+		const vbucks = interaction.options.getString('vbucks');
 
-        const user = await Users.findOne({where: { discordId:  selectedUserId }});
-        if (!user) return interaction.reply({ content: "That user does not own an account", ephemeral: true });
+		//Await user then find profile
+		const user = await Users.findOne({ where: { discordId: selectedUserId } });
+		if (!user) return interaction.reply({ content: "User not found", ephemeral: true });
+		
+		const profile = await Profiles.findOne({ where: { accountId: user.accountId } }).then((profile) => {
+			if (!profile) return interaction.reply({ content: "User profile not found", ephemeral: true });
+			
+			const profiles = profile.profiles; // Assuming 'common_core' is the JSON column name
+			currentVbucks = parseInt(profiles.common_core.items['Currency:MtxPurchased'].quantity.toString());
+			profiles.common_core.items['Currency:MtxPurchased'].quantity = currentVbucks + parseInt(vbucks);
+			newVbucks = profiles.common_core.items['Currency:MtxPurchased'].quantity;
+			Profiles.update({ profiles: profiles }, { where: { accountId: user.accountId } });
 
-		const vbucks:number = interaction.options.getString('vbucks');
-        
-        //@ts-ignore
-        await Profiles.updateOne( { accountId: user.accountId }, { $inc: { 'profiles.common_core.items.Currency:MtxPurchased.quantity' : parseInt(vbucks || 0) } });
+	
+		});
+		
+
+		
 
 		const embed = new EmbedBuilder()
-			.setTitle("vBucks changed")
-			.setDescription("Successfully changed the amount of vbucks for <@" + selectedUserId + "> by " + vbucks)
-			.setColor("#2b2d31")
-			.setFooter({
-				text: "Momentum",
-				iconURL: "https://cdn.discordapp.com/app-assets/432980957394370572/1084188429077725287.png",
-			})
-			.setTimestamp();
+		.setTitle("vBucks changed")
+		.setDescription("Successfully changed the amount of vbucks for <@" + selectedUserId + "> by " + vbucks + " the account now has " + newVbucks + " vbucks")
+		.setColor("#2b2d31")
+		.setFooter({
+			text: "Momentum",
+			iconURL: "https://cdn.discordapp.com/app-assets/432980957394370572/1084188429077725287.png",
+		})
+		.setTimestamp();
 
-            await interaction.reply({ embeds: [embed], ephemeral: true });
+		await interaction.reply({ embeds: [embed], ephemeral: true });
+
+        
+        // Find the the users profile and update the vbucks
 
 	},
 };
